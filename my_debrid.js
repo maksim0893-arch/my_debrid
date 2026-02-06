425ef358059a8f2eb492c95fd5e01201ea9a18ee
maksim0893-arch <maksim080893@gmail.com>
2026-02-06 13:36:11 +0200

    Update my_debrid.js 2

--- my_debrid.js
+++ my_debrid.js
@@ -8,27 +8,18 @@
 
         this.start = function () {
             this.addSettings();
-            this.interceptTorrents();
+            this.intercept();
+            console.log('AllDebrid: Plugin fully loaded');
         };
 
-        // Додаємо пункт у налаштування Lampa
         this.addSettings = function () {
-            var _this = this;
             Lampa.Settings.listener.follow('open', function (e) {
-                if (e.name == 'ts') { // Додаємо в розділ "Торренти" або створюємо свій
-                    var item = $('<div class="settings-param selector" data-type="toggle" data-name="alldebrid_use">' +
-                        '<div class="settings-param__name">Використовувати AllDebrid</div>' +
-                        '<div class="settings-param__value"></div>' +
-                    '</div>');
-                    
-                    var key_item = $('<div class="settings-param selector" data-type="input" data-name="alldebrid_apikey">' +
-                        '<div class="settings-param__name">AllDebrid API Key</div>' +
-                        '<div class="settings-param__value">Натисніть, щоб ввести</div>' +
-                    '</div>');
+                if (e.name == 'interface') { // Додаємо в розділ "Інтерфейс", там легше знайти
+                    var item = $('<div class="settings-param selector" data-name="alldebrid_use" data-type="toggle"><div class="settings-param__name">Використовувати AllDebrid</div><div class="settings-param__value"></div></div>');
+                    var key_item = $('<div class="settings-param selector" data-name="alldebrid_apikey" data-type="input"><div class="settings-param__name">AllDebrid API Key</div><div class="settings-param__value">Натисніть для вводу</div></div>');
 
                     e.body.find('.settings-param:last').after(item).after(key_item);
-                    
-                    // Обробка вводу ключа
+
                     key_item.on('hover:enter', function () {
                         Lampa.Input.edit({
                             value: Lampa.Storage.get('alldebrid_apikey', ''),
@@ -37,7 +28,6 @@
                             if (new_value) {
                                 Lampa.Storage.set('alldebrid_apikey', new_value);
                                 Lampa.Noty.show('Ключ збережено');
-                                Lampa.Settings.update();
                             }
                         });
                     });
@@ -45,70 +35,51 @@
             });
         };
 
-        // Перехоплення кліку на торрент
-        this.interceptTorrents = function () {
+        this.intercept = function () {
             var _this = this;
-            
-            // Підміняємо стандартну функцію запуску TorrServe
-            var old_play = Lampa.Torserve.stream;
-            
+            // Перехоплюємо запуск потоку
+            var original_stream = Lampa.Torserve.stream;
             Lampa.Torserve.stream = function (item) {
-                if (!Lampa.Storage.field('alldebrid_use')) {
-                    return old_play(item); // Якщо вимкнено — працює як зазвичай
+                if (!Lampa.Storage.field('alldebrid_use') || !Lampa.Storage.get('alldebrid_apikey')) {
+                    return original_stream(item);
                 }
 
-                var key = Lampa.Storage.get('alldebrid_apikey', '');
-                if (!key) {
-                    Lampa.Noty.show('Введіть API Key AllDebrid у налаштуваннях');
-                    return;
-                }
-
+                var key = Lampa.Storage.get('alldebrid_apikey');
                 var magnet = item.link || item.magnet;
-                Lampa.Loading.show('Дебрид: Перевірка посилання...');
 
-                // 1. Відправляємо магнет в AllDebrid
-                var upload_url = api_url + 'magnet/upload?agent=' + agent + '&apikey=' + key + '&magnets[]=' + encodeURIComponent(magnet);
+                Lampa.Loading.show('AllDebrid: Надсилаємо магнет...');
+
+                var u_url = api_url + 'magnet/upload?agent=' + agent + '&apikey=' + key + '&magnets[]=' + encodeURIComponent(magnet);
                 
-                network.silent(upload_url, function (data) {
-                    if (data.status === 'success' && data.data.magnets[0]) {
-                        var magnet_id = data.data.magnets[0].id;
-                        
-                        // 2. Отримуємо статус і пряме посилання
-                        _this.checkStatus(magnet_id, key, item);
+                network.silent(u_url, function (json) {
+                    if (json.status === 'success' && json.data.magnets[0]) {
+                        _this.check(json.data.magnets[0].id, key, item);
                     } else {
                         Lampa.Loading.hide();
-                        Lampa.Noty.show('AllDebrid: Помилка завантаження');
+                        Lampa.Noty.show('Помилка завантаження магніту');
                     }
-                }, function() {
+                }, function(){
                     Lampa.Loading.hide();
                     Lampa.Noty.show('Помилка мережі API');
                 });
             };
         };
 
-        this.checkStatus = function (id, key, item) {
-            var _this = this;
-            var status_url = api_url + 'magnet/status?agent=' + agent + '&apikey=' + key + '&id=' + id;
-            
-            network.silent(status_url, function (res) {
+        this.check = function (id, key, item) {
+            var s_url = api_url + 'magnet/status?agent=' + agent + '&apikey=' + key + '&id=' + id;
+            network.silent(s_url, function (res) {
                 Lampa.Loading.hide();
-                if (res.status === 'success') {
-                    var m = res.data.magnets;
-                    if (m.status === 'Ready') {
-                        // Якщо файлів кілька, беремо перший найбільший відеофайл
-                        var link = m.links[0].link;
-                        
-                        Lampa.Player.play({
-                            url: link,
-                            title: item.title
-                        });
-                        
-                        Lampa.Player.callback(function () {
-                            Lampa.Controller.toggle('content');
-                        });
-                    } else {
-                        Lampa.Noty.show('Файл ще не закешований (Статус: ' + m.status + ')');
-                    }
+                if (res.status === 'success' && res.data.magnets.status === 'Ready') {
+                    var final_link = res.data.magnets.links[0].link;
+                    Lampa.Player.play({
+                        url: final_link,
+                        title: item.title
+                    });
+                    Lampa.Player.callback(function () {
+                        Lampa.Controller.toggle('content');
+                    });
+                } else {
+                    Lampa.Noty.show('Файл не в кеші або ще завантажується');
                 }
             });
         };
